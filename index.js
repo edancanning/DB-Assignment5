f4480ed1807697fdb46f9550148f41289dc5202b
Edan Canning <edancanning@ufl.edu>
2017-12-07 12:29:55 -0500

    Adds info update form for flowers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
     var $table = $("#table");
     var $sightings = $("#sightings");
+    var $info = $("#info");
 
     $.ajax({
         type: "GET",
@@ -19,19 +20,37 @@ $(document).ready(function() {
 
             var $flower = $(markup);
             $flower.on("click", function() {
+                var $this = $(this);
                 $("#current").attr("id", "");
-                $(this).attr("id", "current");
-                var comname = $(
-                    $(this)
-                        .find(".cell")
-                        .get(2)
-                ).text();
+                $this.attr("id", "current");
+                var genus = $($this.find(".cell").get(0)).text();
+                var species = $($this.find(".cell").get(1)).text();
+                var comname = $($this.find(".cell").get(2)).text();
                 var markupSightingsTable =
                     " <h2>Sightings (last 10)</h2> <h3 id='comname'>" +
                     comname +
                     "</h3> <div id='tableSightings' class='table'> <div class='row header'><div class='cell'> Person </div> <div class='cell'> Location </div> <div class='cell'> Sighted </div> </div> </div>";
+                var markupInfo =
+                    "<h2>Edit info</h2> <h3>Comname</h3> <form id='info-form' class='cf'> <div class='input'> <label for='input-Genus'>Genus</label> <input type='text' id='input-Genus' value='" +
+                    genus +
+                    "'> </div> <div class='input'> <label for='input-species'>Species</label> <input type='text' id='input-species' value='" +
+                    species +
+                    "'> </div> <div class='input'> <label for='input-comname'>Common Name</label> <input type='text' id='input-name' value='" +
+                    comname +
+                    "'> </div> </form>";
+                var $markupInfo = $(markupInfo);
+                var $submit = $(
+                    "<input type='submit' value='Submit' id='input-submit'>"
+                );
+                $submit.on("click", function(e) {
+                    e.preventDefault();
+                    console.log("submit");
+                });
                 $sightings.empty();
                 $sightings.append(markupSightingsTable);
+                $info.empty();
+                $info.append(markupInfo);
+                $("#info-form").append($submit);
                 $.ajax({
                     type: "GET",
                     url: "/sightings/" + comname.trim(),
