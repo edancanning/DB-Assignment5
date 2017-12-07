55a8b4dcf66671c294cc685d698a58ef7cefcd23
Edan Canning <edancanning@ufl.edu>
2017-12-07 14:05:35 -0500

    fix

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ $(document).ready(function() {
                     comname +
                     "</h3> <div id='tableSightings' class='table'> <div class='row header'><div class='cell'> Person </div> <div class='cell'> Location </div> <div class='cell'> Sighted </div> </div> </div>";
                 var markupInfo =
-                    "<h2>Edit info</h2> <h3>" +
+                    "<h2>Edit Info</h2> <h3>" +
                     comname +
                     "</h3> <form id='info-form' class='cf'> <div class='input'> <label for='input-genus'>Genus</label> <input type='text' id='input-genus' value='" +
                     genus +
@@ -57,11 +57,25 @@ $(document).ready(function() {
                     "'> </div> <div class='input'> <label for='input-comname'>Common Name</label> <input type='text' id='input-comname' value='" +
                     comname +
                     "'> </div> </form>";
+                var markupInsert =
+                    "<h2>Insert New Sighting</h2> <h3>" +
+                    comname +
+                    "</h3> <form id='insert-form' class='cf'> <div class='input'> <label for='input-person'>Person</label> <input type='text' id='input-person' value='" +
+                    genus +
+                    "'> </div> <div class='input'> <label for='input-location'>Location</label> <input type='text' id='input-location' value='" +
+                    species +
+                    "'> </div> <div class='input'> <label for='input-sighted'>Sighted</label> <input type='text' id='input-sighted' value='" +
+                    comname +
+                    "'> </div> </form>";
+                var $markupInsert = $(markupInsert);
                 var $markupInfo = $(markupInfo);
-                var $submit = $(
+                var $update = $(
                     "<input type='submit' value='Update' id='input-submit'>"
                 );
-                $submit.on("click", function(e) {
+                var $submit = $(
+                    "<input type='submit' value='Submit' id='input-submit'>"
+                );
+                $update.on("click", function(e) {
                     e.preventDefault();
                     var inputGenus = $("#input-genus").val();
                     var inputSpecies = $("#input-species").val();
@@ -86,8 +100,10 @@ $(document).ready(function() {
                 $sightings.empty();
                 $sightings.append(markupSightingsTable);
                 $info.empty();
+                $info.append(markupInsert);
                 $info.append(markupInfo);
-                $("#info-form").append($submit);
+                $("#info-form").append($update);
+                $("#insert-form").append($submit);
                 $.ajax({
                     type: "GET",
                     url: "/sightings/" + comname.trim(),
